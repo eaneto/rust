93a9372aab24fd194e4b61a09831b39f033cd1d8
Edison Neto <ednetoali@gmail.com>
2020-11-17 18:00:27 -0300

    option and match

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
+struct PersonContainer {
+    people: Vec<Person>
+}
 struct Person{
+    id: u128,
     name: String,
 }
 
 impl Person {
     pub fn say_hi(self) {
-        println!("Hi, my name is {}", self.name)
+        println!("Hi, my name is {} my id is {}", self.name, self.id)
+    }
+}
+
+impl PersonContainer {
+    pub fn find_by_id(self, id: u128) -> Option<Person> {
+        for person in self.people {
+            if person.id == id {
+                return Some(person)
+            }
+        }
+        return None
     }
 }
 
 fn main() {
-    let edison = Person{name: String::from("Edison")};
-    edison.say_hi();
+    let edison = Person{
+        id: 123,
+        name: String::from("Edison")
+    };
+    let id = edison.id;
+    let mut people = Vec::new();
+    people.push(edison);
+
+    let container = PersonContainer{people};
+
+    match container.find_by_id(id) {
+        Some(person) => person.say_hi(),
+        None => println!("not found")
+    }
 }
