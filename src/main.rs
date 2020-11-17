81333a698a65b3f75fb1355428316e885b8bcfdc
Edison Neto <ednetoali@gmail.com>
2020-11-17 17:39:18 -0300

    Basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Person{
+    name: String,
+}
+
+impl Person {
+    pub fn say_hi(self) {
+        println!("Hi, my name is {}", self.name)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let edison = Person{name: String::from("Edison")};
+    edison.say_hi();
 }
