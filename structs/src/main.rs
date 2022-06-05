09a88da809f02903985e912f23a30288712b9db3
Edison Neto <edison.aguiar.neto@gmail.com>
2022-06-05 18:06:36 -0300

    Structs: Chapter 5

---
+++ structs/src/main.rs
@@ -0,0 +1,37 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn main() {
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    println!("{}", user2.username);
+
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
