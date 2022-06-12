3f4c6b6e49dec8eeefcd210c8ff4a5c572d7bbb0
Edison Neto <edison.aguiar.neto@gmail.com>
2022-06-12 19:52:46 -0300

    Error handling. Chapter 9

---
+++ recoverable-errors/src/main.rs
@@ -0,0 +1,74 @@
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let _f = File::open("hello.txt")?;
+
+    Ok(())
+}
+
+fn _read_files() {
+    let f = File::open("hello.txt");
+
+    let _f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    // Using closures instead of match
+    let _f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+fn _read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn _slim_read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn _slimmer_read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn _even_slimmer_read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn _last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
