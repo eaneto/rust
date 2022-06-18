e2f26d46c10c818585836f8ecfea9d9f40bee060
Edison Neto <edison.aguiar.neto@gmail.com>
2022-06-18 19:25:04 -0300

    Update minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,11 @@
-use std::{env, error::Error, fs};
+use std::{error::Error, fs};
 
 pub struct Config {
     pub query: String,
     pub filename: String,
-    pub case_sensitive: bool,
+    pub ignore_case: bool,
+    pub invert_match: bool,
+    pub line_number: bool,
 }
 
 impl Config {
@@ -14,12 +16,31 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        let mut ignore_case = false;
+        let mut line_number = false;
+        let mut invert_match = false;
+
+        if args.len() > 3 {
+            let options = &args[3];
+            if options.contains("i") {
+                ignore_case = true;
+            }
+
+            if options.contains("n") {
+                line_number = true;
+            }
+
+            if options.contains("v") {
+                invert_match = true;
+            }
+        }
 
         Ok(Config {
             query,
             filename,
-            case_sensitive,
+            ignore_case,
+            line_number,
+            invert_match,
         })
     }
 }
@@ -27,10 +48,20 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    let results = if config.case_sensitive {
-        search(&config.query, &contents)
+    let results = if config.ignore_case {
+        search_case_insensitive(
+            &config.query,
+            &contents,
+            config.invert_match,
+            config.line_number,
+        )
     } else {
-        search_case_insensitive(&config.query, &contents)
+        search(
+            &config.query,
+            &contents,
+            config.invert_match,
+            config.line_number,
+        )
     };
 
     for line in results {
@@ -40,25 +71,63 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(
+    query: &str,
+    contents: &'a str,
+    invert_match: bool,
+    line_number: bool,
+) -> Vec<String> {
     let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
+    for (number, line) in contents.lines().enumerate() {
+        if invert_match {
+            if !line.contains(&query) {
+                if line_number {
+                    results.push(format!("{}:{}", number + 1, line));
+                } else {
+                    results.push(format!("{}", line));
+                }
+            }
+        } else {
+            if line.contains(&query) {
+                if line_number {
+                    results.push(format!("{}:{}", number + 1, line));
+                } else {
+                    results.push(format!("{}", line));
+                }
+            }
         }
     }
 
     return results;
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+    invert_match: bool,
+    line_number: bool,
+) -> Vec<String> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
+    for (number, line) in contents.lines().enumerate() {
+        if invert_match {
+            if !line.to_lowercase().contains(&query) {
+                if line_number {
+                    results.push(format!("{}:{}", number + 1, line));
+                } else {
+                    results.push(format!("{}", line));
+                }
+            }
+        } else {
+            if line.to_lowercase().contains(&query) {
+                if line_number {
+                    results.push(format!("{}:{}", number + 1, line));
+                } else {
+                    results.push(format!("{}", line));
+                }
+            }
         }
     }
 
@@ -78,8 +147,42 @@ safe, fast, productive.
 Pick Three.
 Duct tape.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents, false, false)
+        );
     }
+
+    #[test]
+    fn case_sensitive_invert_match() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick Three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["Rust:", "Pick Three.", "Duct tape."],
+            search(query, contents, true, false)
+        );
+    }
+
+    #[test]
+    fn case_sensitive_with_line_number() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick Three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["2:safe, fast, productive."],
+            search(query, contents, false, true)
+        );
+    }
+
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
@@ -91,7 +194,37 @@ Trust me.";
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
+            search_case_insensitive(query, contents, false, false)
+        );
+    }
+
+    #[test]
+    fn case_insensitive_invert_match() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick Three.
+Trust me.";
+
+        assert_eq!(
+            vec!["safe, fast, productive.", "Pick Three."],
+            search_case_insensitive(query, contents, true, false)
+        );
+    }
+
+    #[test]
+    fn case_insensitive_with_line_number() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick Three.
+Trust me.";
+
+        assert_eq!(
+            vec!["1:Rust:", "4:Trust me."],
+            search_case_insensitive(query, contents, false, true)
         );
     }
 }
