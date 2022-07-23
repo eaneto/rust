f6ebcdb5ce2d99f042976ba7ef4ebc680895dccf
Edison Neto <edison.aguiar.neto@gmail.com>
2022-07-22 23:09:17 -0300

    Deref. Chapter 15.2

---
+++ deref/src/main.rs
@@ -0,0 +1,41 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    // Defines an associated type for Deref.
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}", name)
+}
+
+fn main() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("edison"));
+    hello(&m);
+    // syntatic sugar for:
+    // hello(&(*m)[..]);
+}
