b3fde06ceb15412520bb8db1024e75ca3c2fb0d6
Edison Neto <edison.aguiar.neto@gmail.com>
2023-05-13 16:17:23 -0300

    Update web server to use tokio instead of a custom thread pool

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,44 +1,38 @@
-use std::{
-    fs,
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
-};
+use std::{fs, thread, time::Duration};
 
-use web_server::ThreadPool;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = match TcpListener::bind("127.0.0.1:7878") {
+#[tokio::main]
+async fn main() {
+    let listener = match TcpListener::bind("127.0.0.1:7878").await {
         Ok(listener) => listener,
         Err(_) => panic!("Unable to bind port 7878"),
     };
-    let pool = match ThreadPool::new(8) {
-        Ok(pool) => pool,
-        Err(_) => panic!("Invalid number of threads for the thread pool."),
-    };
 
-    for stream in listener.incoming() {
-        let stream = match stream {
-            Ok(stream) => stream,
-            Err(_) => continue,
-        };
-        match pool.execute(|| {
-            handle_connection(stream);
-        }) {
-            Ok(_) => continue,
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                tokio::spawn(async move {
+                    handle_connection(stream).await;
+                });
+            }
             Err(_) => continue,
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut request_line = String::new();
+    let result = buf_reader.read_line(&mut request_line).await;
+    if result.is_err() {
+        return;
+    }
 
     let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
+        "GET / HTTP/1.1\r\n" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1\r\n" => {
             thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "hello.html")
         }
@@ -49,5 +43,5 @@ fn handle_connection(mut stream: TcpStream) {
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    let _ = stream.write_all(response.as_bytes()).await;
 }
